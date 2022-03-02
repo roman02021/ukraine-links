324beea8d1347ffc20c823f8d0a4a1405b907a66
roman02021 <roman.zemanik@revolware.com>
2022-03-02 21:13:15 +0100

    correct icon approach

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import LinkBtn from './components/LinkBtn.jsx';
-
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <LinkBtn text="ahahahha" link="https://fontawesome.com/v5/docs/web/use-with/react" icon="coffe"></LinkBtn>
+      <LinkBtn text="ahahahha" link="https://fontawesome.com/v5/docs/web/use-with/react" icon={faCoffee}></LinkBtn>
     </div>
   );
 }
--- src/components/LinkBtn.jsx
+++ src/components/LinkBtn.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const StyledBtnLink = styled.a`
     color: 'black'
 `;
 
 function LinkBtn({link, text, icon}) {
+
+  console.log(link, text, icon);
   return (
     <StyledBtnLink href={link}><FontAwesomeIcon icon={icon} />{text}</StyledBtnLink>
   )
